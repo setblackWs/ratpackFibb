274567465cf3839787449b7016ce05b0330e3aa7
jarekratajski <jratajski@gmail.com>
2017-05-18 09:16:09 +0200

    200 threads

--- src/main/java/pl/setblack/fibbo/FibboServer.java
+++ src/main/java/pl/setblack/fibbo/FibboServer.java
@@ -19,18 +19,41 @@ public class FibboServer {
     }
 
     public static RatpackServer createServer() throws Exception {
+        final HttpClient httpClient = HttpClient.of(rs -> rs.readTimeout(Duration.ofMinutes(2)));
+
 
         return RatpackServer.start(server -> server
                 .serverConfig(cfg ->
                         cfg
-                                .development(false)
-                                .threads(1)
+                                .development(true)
+                                .threads(200)
+                                .port(5050)
                                 .connectTimeoutMillis(60 * 1000))
                 .handlers(chain -> chain
                         .prefix("fibbo", fibbo
-                                -> fibbo.get(":n",
-                                ctx ->ctx.render("hello!")))));
-
+                                -> fibbo.get(":n", // URL example: /fibbo/5
+                                ctx -> {
+                                    long n = Long.parseLong(ctx.getPathTokens().get("n"));
+                                    if (n <= 2) {
+                                        ctx.render(String.valueOf(n));
+                                    } else {
+                                        Promise<Long> n1 = requestFibb(n - 1, httpClient);
+                                        Promise<Long> n2 = requestFibb(n - 2, httpClient);
+                                        Promise<Long> resultLong = n1.flatMap(x -> n2.map(y -> y + x));
+                                        Promise<String> result = resultLong.map(r -> String.valueOf(r));
+                                        ctx.render(result);
+                                    }
+                                }
+                        ))));
     }
 
+    private static Promise<Long> requestFibb(long n, HttpClient httpClient) throws URISyntaxException {
+        try {
+
+            return httpClient.get(new URI("http://localhost:5050/fibbo/" + n))
+                    .map(resp -> Long.parseLong(resp.getBody().getText()));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
